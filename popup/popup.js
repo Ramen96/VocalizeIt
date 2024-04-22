b49f234d6532896e878dbbac1dc9db837f2c9775
Ramen96 <eric.worsfold@gmail.com>
2024-04-22 16:48:58 -0400

    added promise to fetch voice_IDs

--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,26 @@
-const buttonClicked = document.getElementById('submit');
+const requestAPI = document.getElementById("submit");
+const getVoiceIds = document.getElementById("submit");
 
-
-
-buttonClicked.addEventListener("click", () => {
+requestAPI.addEventListener("click", () => {
   const options = {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: '{"text":"<string>","model_id":"<string>","voice_settings":{"stability":123,"similarity_boost":123,"style":123,"use_speaker_boost":true},"pronunciation_dictionary_locators":[{"pronunciation_dictionary_id":"<string>","version_id":"<string>"}]}'
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: '{"text":"<string>","model_id":"<string>","voice_settings":{"stability":123,"similarity_boost":123,"style":123,"use_speaker_boost":true},"pronunciation_dictionary_locators":[{"pronunciation_dictionary_id":"<string>","version_id":"<string>"}]}',
   };
-  // alert('It Works');
-  fetch('https://api.elevenlabs.io/v1/text-to-speech/{fqnKvlQ2zM2M3Utu5oTR}', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-})
+  fetch(
+    "https://api.elevenlabs.io/v1/text-to-speech/{fqnKvlQ2zM2M3Utu5oTR}",
+    options,
+  )
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+});
 
+getVoiceIds.addEventListener("click", () => {
+  const options = { method: "GET" };
+
+  fetch("https://api.elevenlabs.io/v1/voices", options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+});
