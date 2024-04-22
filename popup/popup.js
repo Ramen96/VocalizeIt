0d5ccfe498bc15988c85c0efdb8979394af4ca35
Ramen96 <eric.worsfold@gmail.com>
2024-04-22 06:11:41 -0400

    created api request, need to get a valid voice_id

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,17 @@
 const buttonClicked = document.getElementById('submit');
 
+
+
 buttonClicked.addEventListener("click", () => {
-  alert('It Works');
+  const options = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: '{"text":"<string>","model_id":"<string>","voice_settings":{"stability":123,"similarity_boost":123,"style":123,"use_speaker_boost":true},"pronunciation_dictionary_locators":[{"pronunciation_dictionary_id":"<string>","version_id":"<string>"}]}'
+  };
+  // alert('It Works');
+  fetch('https://api.elevenlabs.io/v1/text-to-speech/{fqnKvlQ2zM2M3Utu5oTR}', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
 })
+
