ea7fab03dfcffc63b8a5ab79e581fe5c9bf92316
Ramen96 <eric.worsfold@gmail.com>
2024-04-23 04:49:37 -0400

    made progress with api need to read docs on pronunciation_dictionary_id

--- popup/popup.js
+++ popup/popup.js
@@ -1,19 +1,38 @@
 const requestAPI = document.getElementById("submit");
 const getVoiceIds = document.getElementById("submit");
 
+const XI_API_KEY = "YOUR_API_KEY_HERE";
+
 requestAPI.addEventListener("click", () => {
   const options = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: '{"text":"<string>","model_id":"<string>","voice_settings":{"stability":123,"similarity_boost":123,"style":123,"use_speaker_boost":true},"pronunciation_dictionary_locators":[{"pronunciation_dictionary_id":"<string>","version_id":"<string>"}]}',
+    method: 'POST',
+    headers: {
+      "Accept": "application/json",
+      "xi-api-key": XI_API_KEY,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      text: "This text is a test",
+      model_id: "eleven_monolingual_v1",
+      voice_settings: {
+        stability: 0.5,
+        similarity_boost: 0.5,
+        style: 0,
+        use_speaker_boost: true
+      },
+      pronunciation_dictionary_locators: [
+        {
+          pronunciation_dictionary_id: "<string>",
+          version_id: "<string>"
+        }
+      ]
+    })
   };
-  fetch(
-    "https://api.elevenlabs.io/v1/text-to-speech/{fqnKvlQ2zM2M3Utu5oTR}",
-    options,
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
+  
+  fetch(`https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM/stream`, options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
 });
 
 getVoiceIds.addEventListener("click", () => {
