0ddfa57e18a76d86d9c2bccba2d60bf599416910
Ramen96 <eric.worsfold@gmail.com>
2024-04-25 09:42:42 -0400

    fixed error

--- frontend/popup/popup.js
+++ frontend/popup/popup.js
@@ -1,6 +1,9 @@
 // Call api
-const submit = document.getElementById("submit").addEventListener("click", () => {
-    const options = {method: 'POST'};
-      fetch('http://127.0.0.1:5000/api/endpoint', options);
-});
+const submit = document.getElementById("submit");
 
+function apiCall () {
+  const options = {method: 'POST'};
+      fetch('http://127.0.0.1:5000/api/endpoint', options);
+}
+
+submit.addEventListener("click", apiCall());
