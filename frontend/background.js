3f440853158d0fa0f65870e00eeea3a2da9089a8
Ramen96 <eric.worsfold@gmail.com>
2024-05-01 05:45:25 -0400

    Sucessfully fetching url now & moved code from popup.js to background.js to fix errors

---
+++ frontend/background.js
@@ -0,0 +1,51 @@
+// Get URL
+async function getActiveTabUrl () {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (array) => {
+      array && array.length > 0 ?
+      resolve(array[0].url) :
+      reject(new Error("No active tab found"));
+    })
+  })
+}
+
+(async () => { // This is just here for testing purposes.
+  try {        // Next step is to scrape text off websites.
+    const myVar = await getActiveTabUrl();
+    console.log(myVar);
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+
+// Call api
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", () => {
+  const options = {method: 'POST'};
+      fetch('http://127.0.0.1:5000/api/endpoint', options)
+      .then(response => response.blob())
+      .then(blob => {
+        // Create a temporary URL for the Blob
+        const blobUrl = URL.createObjectURL(blob);
+        const audio = new Audio();
+        audio.src = blobUrl;
+        
+        // Play the audio
+        audio.play();
+        
+        // Change the src of the html source tag
+        document.getElementById("ext-audio-player").src = blobUrl;
+
+        // Handle errors
+        audio.addEventListener('error', function() {
+          console.error('Error playing audio.');
+        });
+
+        
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+});
--- frontend/popup/popup.js
+++ frontend/popup/popup.js
@@ -1,50 +1,19 @@
-// function queryActiveTab() {
-//   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-//     return tabs[0].url;
+// Get url
+// async function getActiveTabUrl () {
+//   return new Promise((resolve, reject) => {
+//     chrome.tabs.query({ active: true, currentWindow: true }, (array) => {
+//       array && array.length > 0 ?
+//       resolve(array[0].url) :
+//       reject(new Error("No active tab found"));
+//     })
 //   })
 // }
 
-// let myVar = queryActiveTab();
-
-// console.log("Active tab url: ", myVar);
-
-const activeTabUrl = new Promise((resolve) => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    resolve(tabs[0].url);
-    console.log("URLLLLL", tabs[0].url); // logs the URl
-    // Need to figure out how to properly handle async operation 
-  })
-})
-
-
-console.log(activeTabUrl)
-
-// Call api
-const submit = document.getElementById("submit");
-submit.addEventListener("click", () => {
-  const options = {method: 'POST'};
-      fetch('http://127.0.0.1:5000/api/endpoint', options)
-      .then(response => response.blob())
-      .then(blob => {
-        // Create a temporary URL for the Blob
-        const blobUrl = URL.createObjectURL(blob);
-        const audio = new Audio();
-        audio.src = blobUrl;
-        
-        // Play the audio
-        audio.play();
-        
-        // Change the src of the html source tag
-        document.getElementById("ext-audio-player").src = blobUrl;
-
-        // Handle errors
-        audio.addEventListener('error', function() {
-          console.error('Error playing audio.');
-        });
-
-        
-      })
-      .catch(error => {
-        console.error('Fetch error:', error);
-      });
-});
+// (async () => {
+//   try {
+//     const myVar = await getActiveTabUrl();
+//     console.log(myVar);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
