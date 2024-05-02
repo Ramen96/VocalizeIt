8d482c9e56821e46990b96559005eacaceb5659f
Ramen96 <eric.worsfold@gmail.com>
2024-05-02 03:47:11 -0400

    fixed error

--- frontend/background.js
+++ frontend/background.js
@@ -1,3 +1,5 @@
+// const { Callback } = require("puppeteer");
+
 // Get URL
 async function getActiveTabUrl () {
   return new Promise((resolve, reject) => {
@@ -22,30 +24,32 @@ async function getActiveTabUrl () {
 // Call api
 const submit = document.getElementById("submit");
 
-submit.addEventListener("click", () => {
+function callApi() {
   const options = {method: 'POST'};
-      fetch('http://127.0.0.1:5000/api/endpoint', options)
-      .then(response => response.blob())
-      .then(blob => {
-        // Create a temporary URL for the Blob
-        const blobUrl = URL.createObjectURL(blob);
-        const audio = new Audio();
-        audio.src = blobUrl;
-        
-        // Play the audio
-        audio.play();
-        
-        // Change the src of the html source tag
-        document.getElementById("ext-audio-player").src = blobUrl;
-
-        // Handle errors
-        audio.addEventListener('error', function() {
-          console.error('Error playing audio.');
-        });
-
-        
-      })
-      .catch(error => {
-        console.error('Fetch error:', error);
+    fetch('http://127.0.0.1:5000/api/endpoint', options)
+    .then(response => response.blob())
+    .then(blob => {
+      // Create a temporary URL for the Blob
+      const blobUrl = URL.createObjectURL(blob);
+      const audio = new Audio();
+      audio.src = blobUrl;
+      
+      // Play the audio
+      audio.play();
+      
+      // Change the src of the html source tag
+      document.getElementById("ext-audio-player").src = blobUrl;
+      // Handle errors
+      audio.addEventListener('error', function() {
+        console.error('Error playing audio.');
       });
+      
+    })
+    .catch(error => {
+      console.error('Fetch error:', error);
+    });
+}
+
+submit.addEventListener("click", () => {
+  chrome.action.onClicked.addListener({Callback: callApi()})
 });
