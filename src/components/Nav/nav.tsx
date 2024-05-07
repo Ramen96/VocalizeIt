46b675d0445b9851d428e123a6ee5c5e332a2720
eric <eric@localhost.localdomain>
2024-05-07 05:12:28 -0400

    Added components

---
+++ src/components/Nav/nav.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Component } from "react";
+import '../../assets/tailwind.css'
+
+class Nav extends Component {
+  render() {
+    return <h1 className="text-white text-9xl">Hello World!</h1>;
+  }
+}
+
+export default Nav;
+
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import {createRoot} from 'react-dom/client';
+import Nav from '../components/Nav/nav'
 import '../assets/tailwind.css'
 
-const test = (
+const popup = (
   <div>
-    <h1 className="text-5xl text-green-500 bg-green-50">Hello World</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure nulla rem unde beatae maxime vero in deserunt aperiam magni quo officia, culpa velit, repellendus ea tenetur sunt quae facere error.</p>
-    <img src='pat-back.svg' alt='background'></img>
+    <Nav />
   </div>
 )
 
 const container = document.createElement('div')
 document.body.appendChild(container)
 const root = createRoot(container)
-root.render(test);
+root.render(popup);
