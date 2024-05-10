55a5f6a5bfa439e5b4f8c97ce42f3da3c28095a4
eric <eric@localhost.localdomain>
2024-05-10 01:03:19 -0400

    added backend

---
+++ backend/server.py
@@ -0,0 +1,23 @@
+import os
+import requests
+from flask import Flask, send_file
+from dotenv import load_dotenv
+from api_request_handler import eleven_labs_tts
+
+api = os.getenv("XI_API_KEY")
+voice_id = "21m00Tcm4TlvDq8ikWAM"
+text = "Tim Pool invented a zeppelin" # Changing propts for testing
+
+app = Flask(__name__)
+
+
+@app.route("/api/endpoint", methods=["POST"])
+def call_api():
+  eleven_labs_tts(api, voice_id, text)
+  return send_file("./output/output.mp3")
+  
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
+  
