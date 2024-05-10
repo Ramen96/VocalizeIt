f827797d372c09ee16c8f74d76fc8f0fd864a74c
Ramen96 <eric.worsfold@gmail.com>
2024-05-10 18:41:44 -0400

    configuring puppeteer for webpack

--- webpack.common.js
+++ webpack.common.js
@@ -62,6 +62,9 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
+  externals: {
+    puppeteer: 'commonjs2 puppeteer',
+  },
   optimization: {
     splitChunks: {
       chunks: 'all',
