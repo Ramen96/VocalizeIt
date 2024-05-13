c4a8ed9dc47ea510ae6f4d86c98bcc2f7cfe44ac
eric <eric@localhost.localdomain>
2024-05-13 03:59:54 -0400

    cleaded up code updated iniitalState updated api calls

--- src/App/app.tsx
+++ src/App/app.tsx
@@ -15,66 +15,48 @@ async function getActiveTabUrl () {
   })
 }
 
-// Popup component
-
-
-
-
 (async () => { 
   try {        
     const url = await getActiveTabUrl();
     console.log("URL: ", url);
-    // const testScraper = document.getElementById("test-scraper");
-    // testScraper.addEventListener("click", () => {
-    //   scrapeData(url);
-    // })
   } catch (error) {
     console.error(error);
   }
 })();
 
 const initialState = {
-  Url: ''
+  url: '',
+  voiceId: '',
 }
 
 class App extends Component  {
 
-  iHaveBeenClicked = () => {
-    alert('You clicked me! 84983938')
-  }
   getVoices = () => {
     const options = {method: 'GET'};
     fetch('https://api.elevenlabs.io/v1/voices/', options)
       .then(response => response.json())
+      .then(response => response.voices)
       .then(response => console.log(response))
       .catch(err => console.error(err));
   }
 
-  // callApi = () => {
-  //   const options = {method: 'POST'};
-  //     fetch('http://127.0.0.1:5000/api/endpoint', options)
-  //     .then(response => response.blob())
-  //     .then(blob => {
-  //       // Create a temporary URL for the Blob
-  //       const blobUrl = URL.createObjectURL(blob);
-  //       const audio = new Audio();
-  //       audio.src = blobUrl;
-        
-  //       // Play the audio
-  //       audio.play();
-        
-  //       // Change the src of the html source tag
-  //       // document.getElementById("ext-audio-player").src = blobUrl;
-  //       // Handle errors
-  //       audio.addEventListener('error', function() {
-  //         console.error('Error playing audio.');
-  //       });
-        
-  //     })
-  //     .catch(error => {
-  //       console.error('Fetch error:', error);
-  //     });
-  // }
+  playVoice = () => {
+    const options = {method: 'POST'};
+      fetch('http://127.0.0.1:5000/api/endpoint', options)
+      .then(response => response.blob())
+      .then(blob => {
+        // Create a temporary URL for the Blob
+        const blobUrl = URL.createObjectURL(blob);
+        console.log(blobUrl);
+
+        // const audio = new Audio();
+        // audio.src = blobUrl;
+        // audio.play();
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+  }
 
   render() {
     return (
