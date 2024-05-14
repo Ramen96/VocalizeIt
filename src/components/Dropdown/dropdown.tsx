da027af698f3c27d39fa9a45eb9edc5273d1d4e2
Ramen96 <eric.worsfold@gmail.com>
2024-05-14 04:37:16 -0400

    Troubleshooting TS2339 error

--- src/components/Dropdown/dropdown.tsx
+++ src/components/Dropdown/dropdown.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Component } from "react";
+import React, { Component } from "react";
 import '../../assets/tailwind.css';
 
 class DropDown extends Component {
@@ -17,13 +16,11 @@ class DropDown extends Component {
     const options = {method: 'GET'};
     fetch('https://api.elevenlabs.io/v1/voices/', options)
       .then(response => response.json())
-      .then(response => response.voices)
-      .then(response => {
-        this.setState({voiceName: response});
+      .then(data => {
+        this.setState({voiceArrayPosition: data.voices});
       })
       .catch(err => console.error(err));
   }
-
   // TODO: 
   // 1. Figure out how to update the section dropdown with the voice names dynamically
   //    - Voices are stored in an array containing objects voiceArrayPosition: []
@@ -31,7 +28,8 @@ class DropDown extends Component {
   //      - Display them in the section dropdown 
   // 3. When a voice in the drop down is clicked on update state for voice name, ID and display voiceName as the lable.
   render() {
-    console.log("This.state", this.state);
+    console.log(this.state)
+    // Cant access this.state.voiceArrayPosition keep getting TS2339
     return (
       <div className="sec-center"> 	
         <input className="dropdown" type="checkbox" id="dropdown" name="dropdown"/>
