3ccb8d6ad941e37f6e17f48707cba67c8d267650
Ramen96 <eric.worsfold@gmail.com>
2024-05-15 04:13:02 -0400

    fixed error with react key prop

--- src/App/app.tsx
+++ src/App/app.tsx
@@ -1,56 +1,43 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import Nav from '../components/Nav/nav';
 import Audio from "../components/Audio/audio";
 import DropDown from "../components/Dropdown/dropdown";
 import Submit from "../components/Submit/submit";
 import '../assets/tailwind.css';
 
-const initialState = {
-  url: '',
-  voiceId: '',
-  text: ''
-}
+const App: React.FC = () => {
 
-async function getActiveTabUrl () {
-  return new Promise((resolve, reject) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (array) => {
-      array && array.length > 0 ?
-      resolve(array[0].url) :
-      reject(new Error("No active tab found"));
+  const [url, setUrl] = useState('');
+  const [voiceId, setVoiceId] = useState('');
+  const [text, setText] = useState('');
+  
+  async function getActiveTabUrl () {
+    return new Promise((resolve, reject) => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (array) => {
+        array && array.length > 0 ?
+        resolve(array[0].url) :
+        reject(new Error("No active tab found"));
+      })
     })
-  })
-}
-
-(async () => { 
-  try {        
-    const url = await getActiveTabUrl();
-    console.log("URL: ", url);
-  } catch (error) {
-    console.error(error);
   }
-})();
+  
+  (async () => { 
+    try {        
+      const url = await getActiveTabUrl();
+      console.log("URL: ", url);
+    } catch (error) {
+      console.error(error);
+    }
+  })();
 
-class App extends Component  {
-
-  constructor(props) {
-    super(props);
-    this.state = initialState;
-  }
-
-  logState = () => {
-    console.log(this.state)
-  }
-
-  render() {
-    return (
+    return(
       <div className="flex flex-col items-center h-24">
-        <Nav />
-        <Audio />
-        <DropDown />
-        <Submit />
+        <Nav key={'nav'} />
+        <Audio key={'audio'} />
+        <DropDown key={'dropdown'} />
+        <Submit key={'submit'} />
       </div>
     )
-  }
 }
 
 export default App;
--- src/components/Dropdown/dropdown.tsx
+++ src/components/Dropdown/dropdown.tsx
@@ -5,6 +5,7 @@ const DropDown: React.FC = () => {
   const [voiceId, setVoiceId] = useState('');
   const [voiceName, setVoiceName] = useState('');
   const [voiceArrayPosition, setVoiceArrayPosition] = useState([]);
+  const [nameDisplay, setNameDisplay] = useState('AI Voices');
 
   useEffect(() => {
     const options = { method: 'GET' };
@@ -12,6 +13,7 @@ const DropDown: React.FC = () => {
       .then(response => response.json())
       .then(data => {
         setVoiceArrayPosition(data.voices);
+        console.log(data)
       })
       .catch(err => console.error(err));
   }, []);
@@ -20,12 +22,12 @@ const DropDown: React.FC = () => {
     <div className="sec-center">
       <input className="dropdown" type="checkbox" id="dropdown" name="dropdown" />
       <label className="for-dropdown" htmlFor="dropdown">
-        AI Voices<i className="uil uil-arrow-down ext-icon2"></i>
+        {nameDisplay}<i className="uil uil-arrow-down ext-icon2"></i>
       </label>
       <div className="ext-section-dropdown">
         {voiceArrayPosition.map(voice => (
           <a
-            key={voice.id}
+            key={voice.voice_id}
             href="#"
             className="ext-icon"
             onClick={() => setVoiceName(voice.name)}
@@ -40,80 +42,3 @@ const DropDown: React.FC = () => {
 };
 
 export default DropDown;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { Component } from "react";
-// import '../../assets/tailwind.css';
-
-// interface Voice {
-//   id: string;
-//   name: string;
-// }
-
-// interface State {
-//   voiceId: string;
-//   voiceName: string;
-//   voiceArrayPosition: Voice[];
-// }
-
-// class DropDown extends Component {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       voiceId: '',
-//       voiceName: '',
-//       voiceArrayPosition: []
-//     }
-//   }
-  
-//   componentDidMount(): void {
-//     const options = {method: 'GET'};
-//     fetch('https://api.elevenlabs.io/v1/voices/', options)
-//       .then(response => response.json())
-//       .then(data => {
-//         this.setState({voiceArrayPosition: data.voices});
-//       })
-//       .catch(err => console.error(err));
-//   }
-//   // TODO: 
-//   // 1. Figure out how to update the section dropdown with the voice names dynamically
-//   //    - Voices are stored in an array containing objects voiceArrayPosition: []
-//   // 2. Create function that will loop through the array grabing the voice name and ID 
-//   //      - Display them in the section dropdown 
-//   // 3. When a voice in the drop down is clicked on update state for voice name, ID and display voiceName as the lable.
-//   render() {
-//     // Cant access this.state.voiceArrayPosition keep getting TS2339
-//     console.log(this.state)
-//     return (
-//       <div className="sec-center"> 	
-//         <input className="dropdown" type="checkbox" id="dropdown" name="dropdown"/>
-//         <label className="for-dropdown" htmlFor="dropdown">AI Voices<i className="uil uil-arrow-down ext-icon2"></i></label>
-//         <div className="ext-section-dropdown"> 
-//             <a href="#" className="ext-icon">Voice<i className="uil uil-arrow-right"></i></a>
-//             <a href="#" className="ext-icon">Voice<i className="uil uil-arrow-right"></i></a>
-//             <a href="#" className="ext-icon">Voice<i className="uil uil-arrow-right"></i></a>
-//             <a href="#" className="ext-icon">Voice<i className="uil uil-arrow-right"></i></a>
-//             <a href="#" className="ext-icon">Voice<i className="uil uil-arrow-right"></i></a>
-//             <a href="#" className="ext-icon">Voice<i className="uil uil-arrow-right"></i></a>
-//         </div>
-//     </div>
-//     )
-//   }
-// }
-
-// export default DropDown;
