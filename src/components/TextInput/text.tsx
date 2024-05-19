a29abc1e6284cf55d7fc67d45bc6596c8217688e
Ramen96 <eric.worsfold@gmail.com>
2024-05-18 20:43:11 -0400

    changed bg color

--- src/components/TextInput/text.tsx
+++ src/components/TextInput/text.tsx
@@ -10,8 +10,7 @@ const Text: React.FC<StateProps> = ({ text }) => {
 
   return (
     <div className="flex justify-center">
-      <input type="email" id="helper-text" aria-describedby="helper-text-explanation" className="text-sm m-5 w-80 h-52 overflow-y-auto rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-zinc-950 border-gray-200 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Paste some text or type something..."></input>
-      <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500 dark:text-gray-400"></p>
+      <input type="email" id="helper-text" aria-describedby="helper-text-explanation" className="text-sm m-5 w-80 h-56 overflow-y-auto rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-zinc-950 border-gray-200 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Paste some text or type something..."></input>
     </div>
   )
 }
