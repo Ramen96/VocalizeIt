4b231c8a8b1bbe7c3370d7d7a04917c3fd4a0050
Ramen96 <eric.worsfold@gmail.com>
2024-05-18 20:29:27 -0400

    made input feild larger

--- src/components/TextInput/text.tsx
+++ src/components/TextInput/text.tsx
@@ -10,7 +10,7 @@ const Text: React.FC<StateProps> = ({ text }) => {
 
   return (
     <div className="flex justify-center">
-      <input type="email" id="helper-text" aria-describedby="helper-text-explanation" className="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Paste some text"></input>
+      <input type="email" id="helper-text" aria-describedby="helper-text-explanation" className="text-sm m-5 w-80 h-52 overflow-y-auto rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-950 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Paste some text or type something..."></input>
       <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500 dark:text-gray-400"></p>
     </div>
   )
