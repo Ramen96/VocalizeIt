6ecb1781bc6143f13e9eda23ab37a4f4e9dea295
Ramen96 <eric.worsfold@gmail.com>
2024-06-10 04:59:14 -0400

    updated nav hight

--- src/components/Nav/nav.tsx
+++ src/components/Nav/nav.tsx
@@ -14,7 +14,7 @@ const Nav: React.FC<StateProps> = ({ setSignInState, signInState }) => {
 
   return(
     <div className="flex items-center justify-between w-full">
-      <div className="flex items-center justify-center h-20 w-full mr-5">
+      <div className="flex items-center justify-center h-16 w-full mr-5">
         <h1 className="ml-5 text- text-xl font-press2start tracking-widest text-green-500 font-mono font-thin">VocalizeIt</h1>
       </div>
       <div className="flex items-center justify-end w-full mr-5">
